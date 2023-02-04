524b3247147c05077033c1de4d8568872fde8849
Zhongdong Yang <zhongdong_y@outlook.com>
2023-02-04 13:43:53 +0800

    Add: support Baidu mobile sitemap protocal

--- libs/sitemap.py
+++ libs/sitemap.py
@@ -12,6 +12,7 @@ def generate_sitemaps(tag_index: TagIndexCollection, *, archives=None):
     root_element.attrib[
         'xsi:schemaLocation'] = "http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
     root_element.attrib['xmlns'] = "http://www.sitemaps.org/schemas/sitemap/0.9"
+    root_element.attrib['xmlns:mobile'] = "http://www.baidu.com/schemas/sitemap-mobile/1/"
 
     config = load_config()
     url_root = ""
@@ -30,6 +31,7 @@ def generate_sitemaps(tag_index: TagIndexCollection, *, archives=None):
         # Include tag page
         tag_element = ET.SubElement(root_element, "url")
         ET.SubElement(tag_element, "loc").text = url_root + urllib.parse.quote(f'/tag/{tag}/')
+        ET.SubElement(tag_element, "mobile:mobile", dict(type="pc,mobile"))
         ET.SubElement(tag_element, "lastmod").text = datetime.now().strftime("%Y-%m-%d")
         ET.SubElement(tag_element, "changefreq").text = "daily"
         ET.SubElement(tag_element, "priority").text = "0.8"
@@ -48,15 +50,18 @@ def generate_sitemaps(tag_index: TagIndexCollection, *, archives=None):
         posts = list(sorted(deduplicated_posts, key=lambda p: p.preamble.updated_at, reverse=True))
         for post in posts:
             post.name = post.name.replace('\\', '/')  # Unescape backslash
-            if post.preamble.permanent_link is not None and len(post.preamble.permanent_link)>0:
+            if post.preamble.permanent_link is not None and len(post.preamble.permanent_link) > 0:
                 post_element = ET.SubElement(root_element, "url")
-                ET.SubElement(post_element, "loc").text = url_root + urllib.parse.quote(f'/post/{post.preamble.permanent_link}/')
+                ET.SubElement(post_element, "loc").text = url_root + urllib.parse.quote(
+                    f'/post/{post.preamble.permanent_link}/')
+                ET.SubElement(post_element, "mobile:mobile", dict(type="pc,mobile"))
                 ET.SubElement(post_element, "lastmod").text = post.preamble.updated_at.strftime("%Y-%m-%d")
                 ET.SubElement(post_element, "changefreq").text = "weekly"
                 ET.SubElement(post_element, "priority").text = "0.8"
 
             post_element = ET.SubElement(root_element, "url")
             ET.SubElement(post_element, "loc").text = url_root + urllib.parse.quote(f'/post/{post.name}/')
+            ET.SubElement(post_element, "mobile:mobile", dict(type="pc,mobile"))
             ET.SubElement(post_element, "lastmod").text = post.preamble.updated_at.strftime("%Y-%m-%d")
             ET.SubElement(post_element, "changefreq").text = "weekly"
             ET.SubElement(post_element, "priority").text = "0.8"
@@ -64,31 +69,35 @@ def generate_sitemaps(tag_index: TagIndexCollection, *, archives=None):
     # Include tags page
     tags_element = ET.SubElement(root_element, "url")
     ET.SubElement(tags_element, "loc").text = url_root + f'/tags/'
+    ET.SubElement(tags_element, "mobile:mobile", dict(type="pc,mobile"))
     ET.SubElement(tags_element, "lastmod").text = datetime.now().strftime("%Y-%m-%d")
     ET.SubElement(tags_element, "changefreq").text = "daily"
     ET.SubElement(tags_element, "priority").text = "0.8"
 
     # Include archives page
-    tags_element = ET.SubElement(root_element, "url")
-    ET.SubElement(tags_element, "loc").text = url_root + f'/archives/'
-    ET.SubElement(tags_element, "lastmod").text = datetime.now().strftime("%Y-%m-%d")
-    ET.SubElement(tags_element, "changefreq").text = "daily"
-    ET.SubElement(tags_element, "priority").text = "0.8"
+    archives_element = ET.SubElement(root_element, "url")
+    ET.SubElement(archives_element, "loc").text = url_root + f'/archives/'
+    ET.SubElement(archives_element, "mobile:mobile", dict(type="pc,mobile"))
+    ET.SubElement(archives_element, "lastmod").text = datetime.now().strftime("%Y-%m-%d")
+    ET.SubElement(archives_element, "changefreq").text = "daily"
+    ET.SubElement(archives_element, "priority").text = "0.8"
 
     # Include archives page by year and month
     if archives is not None and len(archives) > 0:
         for year in archives:
-            tags_element = ET.SubElement(root_element, "url")
-            ET.SubElement(tags_element, "loc").text = url_root + f'/archives/{year}/'
-            ET.SubElement(tags_element, "lastmod").text = datetime.now().strftime("%Y-%m-%d")
-            ET.SubElement(tags_element, "changefreq").text = "monthly"
-            ET.SubElement(tags_element, "priority").text = "0.3"
+            archives_element = ET.SubElement(root_element, "url")
+            ET.SubElement(archives_element, "loc").text = url_root + f'/archives/{year}/'
+            ET.SubElement(archives_element, "mobile:mobile", dict(type="pc,mobile"))
+            ET.SubElement(archives_element, "lastmod").text = datetime.now().strftime("%Y-%m-%d")
+            ET.SubElement(archives_element, "changefreq").text = "monthly"
+            ET.SubElement(archives_element, "priority").text = "0.3"
             for month in archives[year]:
-                tags_element = ET.SubElement(root_element, "url")
-                ET.SubElement(tags_element, "loc").text = url_root + f'/archives/{year}/{month}/'
-                ET.SubElement(tags_element, "lastmod").text = datetime.now().strftime("%Y-%m-%d")
-                ET.SubElement(tags_element, "changefreq").text = "monthly"
-                ET.SubElement(tags_element, "priority").text = "0.3"
+                archives_element = ET.SubElement(root_element, "url")
+                ET.SubElement(archives_element, "loc").text = url_root + f'/archives/{year}/{month}/'
+                ET.SubElement(archives_element, "mobile:mobile", dict(type="pc,mobile"))
+                ET.SubElement(archives_element, "lastmod").text = datetime.now().strftime("%Y-%m-%d")
+                ET.SubElement(archives_element, "changefreq").text = "monthly"
+                ET.SubElement(archives_element, "priority").text = "0.3"
 
     tree = ET.ElementTree(root_element)
     tree.write('cached/sitemap.xml', encoding='utf-8', xml_declaration=True)
